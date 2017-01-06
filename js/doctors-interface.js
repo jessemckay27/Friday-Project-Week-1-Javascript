1ef9c8d7460191c5dec44e120af5743eef519480
Jesse <rosecity27@comcast.net>
2017-01-06 15:44:17 -0800

    add styling and functions for interface

--- js/doctors-interface.js
+++ js/doctors-interface.js
@@ -1,15 +1,12 @@
-var apiKey = require('./../.env').apiKey;
+var Doctor = require('./../js/doctors.js').weatherModule;
 
-$("document").ready(function() {
-  
-
-  exports.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result);
-    })
-   .fail(function(error){
-      console.log("fail");
-    });
-  };
+$(document).ready(function() {
+  var currentDoctorObject = new Doctor();
+  $('#get-doctor').submit(function(event) {
+    event.preventDefault();
+    $("#fix-width").empty();
+    var ailment = $('#ailment').val();
+    $('#ailment').val("");
+    currentDoctorObject.getDoctor(ailment);
+  });
 });
---
+++ js/doctors.js
@@ -0,0 +1,20 @@
+var apiKey = require('./../.env').apiKey;
+
+Doctor = function(){
+}
+
+Doctor.prototype.getDoctor = function(ailment) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + ailment + '&skip=0&limit=12&user_key=' + apiKey).then(function(response) {
+    for (i = 0; i < 11; i++) {
+      var doctorImage = response.data[i].profile.image_url;
+      var doctorFirstName = response.data[i].profile.first_name;
+      var doctorLastName = response.data[i].profile.last_name;
+      $("#fix-width").append("<div class='doctor-wrap col-xs-6 col-sm-6 col-md-3'><div class='doctor-box'><div class='doctor-img-wrap'><img src='" + doctorImage + "' alt=''></div><div class='doctor-info-wrap'><div class='doctor-name'>Doctor " + doctorFirstName + " " + doctorLastName + "</div></div></div></div>");
+    }
+
+  }).fail(function(error) {
+    $('.showDoctor').text(error.responseJSON.message);
+  });
+}
+
+exports.weatherModule = Doctor;
